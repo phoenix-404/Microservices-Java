adf2bb96906649b08a3aabf70a8671335df767bf
Sourav Karmakar <sourav.official321@gmail.com>
2025-11-02 10:50:23 +0530

    Retracting feign client implementation

--- QuizService/src/main/java/com/example/QuizService/QuizServiceApplication.java
+++ QuizService/src/main/java/com/example/QuizService/QuizServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 //@EnableFeignClients : configure and enable all Feign Clients
 
 @SpringBootApplication
-@EnableFeignClients
+//@EnableFeignClients
 public class QuizServiceApplication {
 
 	public static void main(String[] args) {
